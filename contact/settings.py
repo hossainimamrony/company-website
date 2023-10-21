893edeed6d95cce79efb7a1b2d1cecd6ef471aa7
kalilinux <kalilinux@kali>
2023-10-22 02:58:30 +0600

    all Done

--- contact/settings.py
+++ contact/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = 'django-insecure-qj%(^p%%2+!+)4uze#ew$p!8&em2-xgte8_!s_!7_vey32k4rj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -62,12 +62,12 @@ WSGI_APPLICATION = 'contact.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-#DATABASES = {
- #   'default': {
-  #      'ENGINE': 'django.db.backends.sqlite3',
-   #     'NAME': BASE_DIR / 'db.sqlite3',
-   #}
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+   }
+}
 
 
 # Password validation
