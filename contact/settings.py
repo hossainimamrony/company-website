e0c08d73e68d43e55f6b511bd3a0e3431a588031
kalilinux <kalilinux@kali>
2023-10-23 00:47:23 +0600

    ALL oks

--- contact/settings.py
+++ contact/settings.py
@@ -63,10 +63,14 @@ WSGI_APPLICATION = 'contact.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#   }
+    'default': {
+       'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'test',
+        'USER': 'team',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+   }
 }
 
 
