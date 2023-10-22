7f03f7b392bbb0ead0781d5250fc778604820b98
kalilinux <kalilinux@kali>
2023-10-22 21:04:07 +0600

    ALL ok

--- contact/settings.py
+++ contact/settings.py
@@ -64,12 +64,8 @@ WSGI_APPLICATION = 'contact.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'testdb',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-       ' PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
    }
 }
 
